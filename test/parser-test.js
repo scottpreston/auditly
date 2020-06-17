888750588c484f5d18f36e5403f9d05474735b1d
Rishi SHeth <sheth19@purdue.edu>
2020-06-16 22:14:15 -0400

    Create parser-test.js

---
+++ test/parser-test.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require("path");
+var assert = require('assert');
+var parse = require('../parser');
+
+describe('Parser', function() {
+
+    it('testing', function(){
+        assert(true);
+    });
+});
