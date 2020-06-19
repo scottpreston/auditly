5f5ccd21c246f70b372bc4268665735d19ab342c
sheth19 <54328232+sheth19@users.noreply.github.com>
2020-06-19 10:23:55 -0400

    Testing and travis (#14)
    * Delete convert.js
    
    * Delete utils.js
    
    * Update parser.js
    
    * Update parser.js
    
    * Update parser.js
    
    * Update package-lock.json
    
    * Update package.json
    
    * Create .travis.yml
    
    * Update package-lock.json
    
    * Update package.json
    
    * Create parser-test.js
    
    * Update parser-test.js

---
+++ test/parser-test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var parser = require('../parser');
+
+describe('Parser', function() {
+    parse
+    it('Place holder for testing until we add more functionality and functions', function(){
+        assert(true);
+    });
+});
